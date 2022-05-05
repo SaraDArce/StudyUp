562ccd7b6ceb192f4e9afffe654d72684cad8603
Sara D'Arce <saradarce@gmail.com>
2022-05-04 23:54:55 -0400

    fine tuning

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -8,40 +8,40 @@ const {
 } = require("../queries/events");
 
 // INDEX
-events.get("/", (req, res) => {
-    const { study_group_id } = req.params;
-    const allEvents = await getAllEvents(study_group_id);
-    if(allEvents[0]) {
-        res.status(200).json(allEvents);
-    } else {
-        res.status(500).json({ error: "server error"});
-    }
+events.get("/", async (req, res) => {
+  const { study_group_id } = req.params;
+  const allEvents = await getAllEvents(study_group_id);
+  if (allEvents[0]) {
+    res.status(200).json(allEvents);
+  } else {
+    res.status(500).json({ error: "server error" });
+  }
 });
 
 // SHOW
 events.get("/:id", async (req, res) => {
-    const { id } = req.params;
-    const event = await getEvent(id);
-    if(event) {
-        res.json(event);
-    } else {
-        res.status(500).json({ error: "server error" })
-    }
-})
+  const { id } = req.params;
+  const event = await getEvent(id);
+  if (event) {
+    res.json(event);
+  } else {
+    res.status(500).json({ error: "server error" });
+  }
+});
 
 events.put("/:id", async (req, res) => {
-    const { id } = req.params;
-    const updatedEvent = await updateEvent(id, req.body);
-    if(updatedEvent.id) {
-        res.status(200).json(updatedEvent);
-    } else {
-      res.status(404).json("Event not found")
-    }
+  const { id } = req.params;
+  const updatedEvent = await updateEvent(id, req.body);
+  if (updatedEvent.id) {
+    res.status(200).json(updatedEvent);
+  } else {
+    res.status(404).json("Event not found");
+  }
 });
 
 events.post("/", async (req, res) => {
-    const event = await newEvent(req.body);
-    res.status(200).json(event);
-})
+  const event = await newEvent(req.body);
+  res.status(200).json(event);
+});
 
 module.exports = events;
--- db/seed.sql
+++ db/seed.sql
@@ -4,7 +4,7 @@ INSERT INTO groups (name, main_focus, date_formed, contact_email) VALUES
 ('Math Masters', 'Math', '12/02/2021 11:00', 'info@studyup.math.netlify.app'),
 ('Science Samurais', 'Science', '12/02/2021 11:00', 'info@studyup.science.netlify.app'),
 ('Gym Rats', 'Gym', '12/02/2021 11:00', 'info@studyup.gym.netlify.app'),
-('English Egalitarians', 'English, "12/02/2021 11:00", "info@studyup.math.netlify.app');
+('English Egalitarians', 'English', '12/02/2021 11:00', 'info@studyup.english.netlify.app');
 
 INSERT INTO events (name, virtual_meeting_link, study_group_id, start_time, end_time, number_of_attendees) VALUES
 ('Monthly Math Mixup', 'https://zoom.us/j/7144663193', '1', '12/02/2021 11:00', '12/02/2021 12:00', 8),
